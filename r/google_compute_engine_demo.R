eaa1856f8912142c0b5b5ab96b36c3858c139874
gmcdonald-sfg <gmcdonald@bren.ucsb.edu>
2022-06-23 14:55:32 -0700

    starting the compute engine demo

---
+++ r/google_compute_engine_demo.R
@@ -0,0 +1,26 @@
+library(googleComputeEngineR)
+
+# Create a Persistent RStudio on Google Compute Engine
+# Following https://cloudyr.github.io/googleComputeEngineR/articles/persistent-rstudio.html
+
+# Give your VM a name
+vm_name <- "gavin-rstudio-server-test"
+
+# n2-standard-8 has 8 cores and 32 GB of RAM
+vm <- gce_vm(name = vm_name, 
+             predefined_type = "n2-standard-8", 
+             template = "rstudio", 
+             username = "gmcdonald", 
+             password = "1234", 
+             dynamic_image = "gcr.io/gcer-public/persistent-rstudio")
+
+gce_get_external_ip(vm_name)
+
+# Navigate browser to {external_ip}:80
+
+# Clone GitHub repo, try shared_demo_analysis_script.R
+
+# Let's stop and delete it
+gce_vm_stop(vm_name)
+
+gce_vm_delete(vm_name)
