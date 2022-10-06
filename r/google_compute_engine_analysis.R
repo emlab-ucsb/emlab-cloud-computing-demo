6419923875fa3cfdcaf615382f315fec9055ead4
gmcdonald-sfg <gmcdonald@bren.ucsb.edu>
2022-10-06 11:33:07 -0700

    remove drive from google vm demo

--- r/google_compute_engine_analysis.R
+++ r/google_compute_engine_analysis.R
@@ -53,34 +53,8 @@ toc()
 # Which is on the team drive at: emlab/data/upsides/Unlumped_ProjectionData.csv
 
 # We could simply upload this file through the GUI - we can demo this
-# Or we could directly access it using `googledrive`
 
-# On our computer or browser, we navigate to that file, and copy the google drive link of the file
-
-
-# The link is: https://drive.google.com/open?id=1-tklB_HWWCShvKZXW8GTrsOXoPf0pm2n&authuser=gmcdonald%40ucsb.edu&usp=drive_fs
-# The file id is therefore: 1-tklB_HWWCShvKZXW8GTrsOXoPf0pm2n
-
-# Authenticate using a Service Account token for non-interactive authentication
-# The json can be downloaded from GCP: https://console.cloud.google.com/iam-admin/serviceaccounts/
-drive_auth(path = file.path(here::here(), "emlab-gcp-841e8a7c268b.json"))
-
-# Let's download the file to a temporary location
-temp_file_name <- tempfile()
-
-# When you fist run this, you will need to authenticate
-# Using your ucsb google account
-drive_get(id="1-tklB_HWWCShvKZXW8GTrsOXoPf0pm2n") %>%
-  drive_download(path = temp_file_name)
-
-# Now we read it in from the temporary location using read_csv
-# This will load the file in our environment
-upsides_data <- temp_file_name %>%
-  read_csv()
-
-upsides_data
-
-# Even faster than Google Drive, you can work with data stored on Google Cloud Storage: https://console.cloud.google.com/storage
+# You can work with data stored on Google Cloud Storage: https://console.cloud.google.com/storage
 # Here's a nice tutorial: https://hydroecology.net/computing-in-the-cloud-with-google-and-rstudio/
 # Load cloud storage package
 library(googleCloudStorageR)
